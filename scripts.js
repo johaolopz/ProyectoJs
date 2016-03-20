e415250f2b565bdeea647eeed201663673aaf442
Johao <johao.lopz.m@gmail.com>
2016-03-20 15:10:32 -0500

    probamos con errores

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-console.log("Inicio de programa");
-alert("Hola Mundo");
+console.log(Inicio de programa");
+alert("Hola Mundo")
 document.write("<h1>Hola amigos, esto es JavaScript!</h1>");
-alert("Gracias por visitar");
+alert("Gracias por visitar);
 console.log("Fin de programa");

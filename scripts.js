85d9f30eddd29a6491f195c0e2eb0a00cfb391b3
Johao <johao.lopz.m@gmail.com>
2016-03-20 15:04:08 -0500

    Empezamos a trabajar con js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+console.log("Inicio de programa");
+alert("Hola Mundo");
+document.write("<h1>Hola amigos, esto es JavaScript!</h1>");
+alert("Gracias por visitar");
+console.log("Fin de programa");

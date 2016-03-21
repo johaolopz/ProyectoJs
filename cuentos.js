faf510de1514bd00aa2a1fbd2f96d2bf3d4c5b20
Johao Lopez <johao.lopz.m@gmail.com>
2016-03-20 21:53:49 -0500

    practicando la concatenacion de strings

---
+++ cuentos.js
@@ -0,0 +1,7 @@
+var verbo = prompt ("Estimado usuario, ingrese un verbo: ");
+var sustantivo = prompt ("Ahora ingrese un sustantivo: ");
+var adjetivo = prompt ("Por último, ingrese un adjetivo: ");
+document.write("<h1>Cuenta cuentos</h1><br>");
+document.write("<p>El "+sustantivo+" "+adjetivo+" "+verbo+" locamente,</p>");
+document.write("<p>porque cuando "+verbo+" el "+sustantivo+" siempre es "+adjetivo+",</p>");
+document.write("<p>por eso el "+sustantivo+" siente que es muy "+adjetivo+" y es así que cada día "+verbo+" que "+verbo+".</p>");

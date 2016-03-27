c9c885fa9c798cd069cc8c2d2fb1931ab233ef90
richard <richard10-00@hotmail.com>
2016-03-27 09:02:06 -0500

    calculadora de tiempo

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
-console.log("Inicio de programa");
-alert("Hola Mundo");
-document.write("<h1>Hola amigos, esto es JavaScript!</h1>");
-alert("Gracias por visitar");
-console.log("Fin de programa");
+var segundosPorMinuto = 60;
+var minutosPorHora = 60;
+var horasPorDia = 24;
+var diasPorMes = 30;
+var mesesPorAño = 12;
+var segundosPorAño = segundosPorMinuto * minutosPorHora * horasPorDia * diasPorMes * mesesPorAño;
+var edadUsuario = prompt ("Estimado usuario, ingrese su edad: ");
+edadUsuario = edadUsuario * segundosPorAño;
+document.write ("<h1>Calculadora del tiempo</h1><p>Usted ha vivido "+edadUsuario+" segundos</p>");

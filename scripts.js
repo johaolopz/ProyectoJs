7ef1c1f35731fce353c2f0549cfb864d8e0cd6c4
Johao Lopez <johao.lopz.m@gmail.com>
2016-03-27 11:16:46 -0500

    condicion if

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
 console.log("Inicio programa");
-var numeroUsuario = prompt("Ingrese un numero: ");
-var aleatorio = Math.floor(Math.random()*numeroUsuario) + 1;
-alert("El numero aleatorio es: "+aleatorio);
-console.log("Programa completado");
+var numeroUsuario = prompt("Ingrese un numero del 1 al 5: ");
+var numeroAdivinar = 3;
+if (parseInt(numeroUsuario) === numeroAdivinar)
+{
+document.write("<p>Adivinaste. Good Job.!!!</p>");
+}
+else
+{
+document.write("<p>No adivinaste, intentalo de nuevo =(</p>");
+}
+console.log("Programa completo");

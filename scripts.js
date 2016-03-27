4d888bf2611aaea1f122f76e5ace75921e9c7539
Johao Lopez <johao.lopz.m@gmail.com>
2016-03-27 12:23:55 -0500

    Uso de funciones

--- scripts.js
+++ scripts.js
@@ -1,46 +1,10 @@
-console.log("Inicio programa");
-//Pedimos un numero al usuario y lo guardamos en una variable
-var numeroUsuario = prompt("Ingrese un numero del 1 al 5: ");
-var numeroAdivinar = 3;
-//Comparamos con if
-if (parseInt(numeroUsuario) === numeroAdivinar)
+function tiraDados()
 {
-	adivinoCorrectamente = true;
+	var dado=Math.floor(Math.random()*6)+1;
+	alert(dado);
+}
 
-}
-//Si no adivina, damos ayuda y volvemos a pedir que ingrese numero
-	else if (parseInt(numeroUsuario) > numeroAdivinar)
-	{
-		alert("El numero que ingresaste es mayor");
-		var nuevoIntento = prompt("Intenta de nuevo, ingresa un numero del 1 al 5: ");
-		if (parseInt(nuevoIntento) === numeroAdivinar)
-		{
-			adivinoCorrectamente = true;
-		}
-		else
-		{
-			adivinoCorrectamente = false;
-		}
-	}
-	else if (parseInt(numeroUsuario) < numeroAdivinar)
-	{ 
-		alert("El numero que ingresaste es menor");
-		var nuevoIntento = prompt("Intenta de nuevo, ingresa un numero del 1 al 5: ");
-		if (parseInt(nuevoIntento) === numeroAdivinar) 
-		{
-			adivinoCorrectamente = true;
-		}
-		else
-		{
-			adivinoCorrectamente = false;
-		}
-	}
-if (adivinoCorrectamente)
-{
-document.write("<p>Adivinaste. Good Job.!!!</p>");	
-}
-else
-{
-document.write("<p>No adivinaste, intentalo de nuevo =(</p>");
-}
-console.log("Programa completo");
+alert("Tiramos 3 dados: ");
+tiraDados();
+tiraDados();
+tiraDados();

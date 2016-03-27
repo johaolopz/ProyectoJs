929794ee116fd0978c3be64eac02902334543004
Johao Lopez <johao.lopz.m@gmail.com>
2016-03-27 11:31:36 -0500

    Juego if/ else-if Adivina el numero con ayuda

--- scripts.js
+++ scripts.js
@@ -3,7 +3,38 @@ var numeroUsuario = prompt("Ingrese un numero del 1 al 5: ");
 var numeroAdivinar = 3;
 if (parseInt(numeroUsuario) === numeroAdivinar)
 {
-document.write("<p>Adivinaste. Good Job.!!!</p>");
+	adivinoCorrectamente = true;
+
+}
+	else if ((parseInt(numeroUsuario) > numeroAdivinar)) 
+	{
+		alert("El numero que ingresaste es mayor");
+		var nuevoIntento = prompt("Intenta de nuevo, ingresa un numero del 1 al 5: ");
+		if ((parseInt(nuevoIntento) === numeroAdivinar)) 
+		{
+			adivinoCorrectamente = true;
+		}
+		else
+		{
+			adivinoCorrectamente = false;
+		}
+	}
+	else if ((parseInt(numeroUsuario) < numeroAdivinar))
+	{ 
+		alert("El numero que ingresaste es menor");
+		var nuevoIntento = prompt("Intenta de nuevo, ingresa un numero del 1 al 5: ");
+		if ((parseInt(nuevoIntento) === numeroAdivinar)) 
+		{
+			adivinoCorrectamente = true;
+		}
+		else
+		{
+			adivinoCorrectamente = false;
+		}
+	}
+if (adivinoCorrectamente)
+{
+document.write("<p>Adivinaste. Good Job.!!!</p>");	
 }
 else
 {

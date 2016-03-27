9d68e39e7ea9cbd478ecb390352272b9cd18d383
Johao Lopez <johao.lopz.m@gmail.com>
2016-03-27 12:04:39 -0500

    comentarios en js condiciones if else

--- scripts.js
+++ scripts.js
@@ -1,16 +1,19 @@
 console.log("Inicio programa");
+//Pedimos un numero al usuario y lo guardamos en una variable
 var numeroUsuario = prompt("Ingrese un numero del 1 al 5: ");
 var numeroAdivinar = 3;
+//Comparamos con if
 if (parseInt(numeroUsuario) === numeroAdivinar)
 {
 	adivinoCorrectamente = true;
 
 }
-	else if ((parseInt(numeroUsuario) > numeroAdivinar)) 
+//Si no adivina, damos ayuda y volvemos a pedir que ingrese numero
+	else if (parseInt(numeroUsuario) > numeroAdivinar)
 	{
 		alert("El numero que ingresaste es mayor");
 		var nuevoIntento = prompt("Intenta de nuevo, ingresa un numero del 1 al 5: ");
-		if ((parseInt(nuevoIntento) === numeroAdivinar)) 
+		if (parseInt(nuevoIntento) === numeroAdivinar)
 		{
 			adivinoCorrectamente = true;
 		}
@@ -19,11 +22,11 @@ if (parseInt(numeroUsuario) === numeroAdivinar)
 			adivinoCorrectamente = false;
 		}
 	}
-	else if ((parseInt(numeroUsuario) < numeroAdivinar))
+	else if (parseInt(numeroUsuario) < numeroAdivinar)
 	{ 
 		alert("El numero que ingresaste es menor");
 		var nuevoIntento = prompt("Intenta de nuevo, ingresa un numero del 1 al 5: ");
-		if ((parseInt(nuevoIntento) === numeroAdivinar)) 
+		if (parseInt(nuevoIntento) === numeroAdivinar) 
 		{
 			adivinoCorrectamente = true;
 		}

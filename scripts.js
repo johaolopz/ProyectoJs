90c0eb1455c54e40b0cc55edc6645a964dd97d46
Johao Lopez <johao.lopz.m@gmail.com>
2016-03-27 15:53:33 -0500

    Ciclo while

--- scripts.js
+++ scripts.js
@@ -1,4 +1,20 @@
-console.log("Inicio programa");
-var dado = Math.floor(Math.random()*6) + 1;
-alert("Dado dice: "+dado);
-console.log("Programa completado");
+function numeroAlAzarEntreLimites(lim1,lim2)
+{
+	var num=Math.floor(Math.random()*(lim1-lim2))+lim2;
+	return num;
+}
+var limiteSuperior=prompt("Ingrese limite superior: ");
+var limiteInferior=prompt("Ingrese limite Inferior: ");
+
+if (isNaN(limiteInferior) || isNaN(limiteSuperior))
+{
+	throw Error("Numero no es valido");
+}
+
+var contador = 0;
+while (contador<10)
+{
+var randomico = numeroAlAzarEntreLimites(parseInt(limiteSuperior),parseInt(limiteInferior));
+document.write(randomico+" ");
+contador++;
+}

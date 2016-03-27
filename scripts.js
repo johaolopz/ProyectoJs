1ac68d67ca95d6eef0bb4bf5bbd38995179f176b
Johao Lopez <johao.lopz.m@gmail.com>
2016-03-27 09:13:49 -0500

    Uso de parseInt

--- scripts.js
+++ scripts.js
@@ -1,9 +1,6 @@
-var segundosPorMinuto = 60;
-var minutosPorHora = 60;
-var horasPorDia = 24;
-var diasPorMes = 30;
-var mesesPorAño = 12;
-var segundosPorAño = segundosPorMinuto * minutosPorHora * horasPorDia * diasPorMes * mesesPorAño;
-var edadUsuario = prompt ("Estimado usuario, ingrese su edad: ");
-edadUsuario = edadUsuario * segundosPorAño;
-document.write ("<h1>Calculadora del tiempo</h1><p>Usted ha vivido "+edadUsuario+" segundos</p>");
+console.log("Inicio programa");
+var peliculasVistas = prompt("Cuantas peliculas haz visto este mes?");
+var seriesVistas = prompt("Cuantas series has visto este mes?");
+var tiempoPantalla = parseInt(peliculasVistas) + parseInt(seriesVistas);
+alert ("Wow! Haz visto " + tiempoPantalla + " veces minimo la pantalla");
+console.log("Programa completado");

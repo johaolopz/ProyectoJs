bb30541baf900dde992a6164ecb86697743df264
Johao <johao.lopz.m@gmail.com>
2016-04-03 16:33:59 -0500

    Ejercicio con matriz de preguntas

--- ejercicios.js
+++ ejercicios.js
@@ -1,30 +1,103 @@
  console.log("Inicio del programa");
- var html="";
- for (var i=1; i<=10; i++)
- 
- function printList (lista)
+
+ alert("Bienvenido al cuestionario Pregunta Preguntón.!!!");
+ var preguntas =[];
+ preguntas.push(["¿Cómo le dicen a Jose Echeverría?","coyote"]);
+ preguntas.push(["A richard Crespin les gusta las:","viudas"]);
+ preguntas.push(["¿A quién dejó botado John Alvarado?","richard"]);
+ preguntas.push(["¿Que apellido es el presidente de uruguay?","vazquez"]);
+ preguntas.push(["¿Cuantos años tiene la china?","15"]);
+
+
+function ImprimirRespuestas(tipoRespuestas)
+{
+	var arregloRespuestas=[];
+	var stringRespuestas='';
+	if (tipoRespuestas.toLowerCase() == "correctas" || tipoRespuestas.toLowerCase() == "incorrectas")
+	{
+		if (tipoRespuestas == "correctas")
+		{
+			arregloRespuestas=correctas;
+		}
+		else
+		{
+			arregloRespuestas=incorrectas;
+		}
+
+		if (arregloRespuestas.length<1)
+		{
+			stringRespuestas= "No hubo respuestas "+tipoRespuestas;
+		}
+
+		for(var i=0; i<arregloRespuestas.length; i++)
+		{
+			stringRespuestas += "<p><b>"+arregloRespuestas[i][0]+"</b><br>"+arregloRespuestas[i][1]+"</p>";
+
+			if(tipoRespuestas=="incorrectas")
+			{
+				stringRespuestas+= "<i>la respuesta era: "+arregloRespuestas[i][2]+"</i>";
+			}
+		}
+	}
+	else
+	{
+		alert("Error, vea la consola!");
+		throw new Error("tipo de respuestas noaceptado");
+	}
+	return stringRespuestas;
+}
+
+
+function imprimirCuestionario()
+{
+	html += "<h3>Respuestas correctas</h3>";
+	html += ImprimirRespuestas("correctas");
+	html += '<h3 style="color:red;">Respuestas Incorrectas</h3>';
+	html += ImprimirRespuestas("incorrectas");
+	return html;
+}
+
+function printHTML(mensaje)
+{
+	var outputDiv = document.getElementById("output");
+	outputDiv.innerHTML = mensaje;
+}
+
+
+ var respuestas = [];
+ var correctas = [];
+ var incorrectas = [];
+
+ //Preguntamnos y procesamos respuestas
+ for (var i=0; i<preguntas.length; i++)
  {
- 	var listHTML = '<ol>';
- 	for (var i=0; i < lista.length; i ++)
+ 	var respuestaSimple;
+ 	do
  	{
- 		html += "<div>"+i+"</div>";
- 		listHTML += '<li>' + lista[i][0] + ',  por: ' + lista[i][1]+'</li>';
-  	}
- 	listHTML += '</ol>';
- 	print(listHTML);
+ 		respuestaSimple = prompt("Responda: "+ preguntas[i][0]);
+ 	} while(respuestaSimple=='')
+
+ 	if(isNaN(respuestaSimple))
+ 	{
+ 		respuestas.push(respuestaSimple.toLowerCase());
+ 	}
+ 	else
+ 	{
+ 		respuestas.push(parseInt(respuestaSimple));
+ 	}
+
+ 	if(respuestas[i]==preguntas[i][1]) 
+ 	{
+ 		correctas.push(preguntas[i]);
+ 	}
+ 	else
+ 	{
+ 		incorrectas.push([preguntas[i][0],respuestas[i],preguntas[i][1]]);
+ 	}
  }
- 
- function print(html)
- {
-  	document.write(html);
- 	console.log("Programa completado"); 
- }
- 
- var playList=[];
- playList.push(["its my life","Jon Bon Jovi"]);
- playList.push(["push your hands","Plush"]);
- playList.push(["david guetta mixes","david guetta"]);
- playList.push(["5ta sinfonia de Bethoven","Bethoven"]);
- playList.push(["move your body","Eiffel 65"]);
- printList(playList);
- console.log("Programa completado");
+
+var html ="<h1>Estimado usuario éste ha sido su cuestionario</h1>";
+html += "<h2>A continuación sus respuestas</h2>";
+html += imprimirCuestionario();
+printHTML(html);
+  console.log("Programa completado");
